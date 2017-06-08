46e17793a2660743f6d53733f9aada5a35b2243a
Mohamed Elmekki <mohamed.elmekki@gmail.com>
2017-06-08 18:16:50 -0400

    Ch7.

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeActivity.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package criminalintent.android.bignerdranch.com.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,6 +11,17 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+
+		//Instantiate new FragmentManager; explicit calls made from here
+		FragmentManager fm = getSupportFragmentManager();
+		//Retrieve CrimeFragment from the FragmentManager
+		Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+		if (fragment == null) {
+			//Create new fragment; include add operation (add fragment to activity); commit
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+		}
 	}
 
 }
