4485843251d4c4c41aa7e56e83447c3fb7b15ec5
Mohamed Elmekki <mohamed.elmekki@gmail.com>
2017-06-09 20:10:19 -0400

    Ch8.

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package criminalintent.android.bignerdranch.com.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
@@ -8,9 +9,14 @@ public class Crime {
 	private UUID mId;
 	//Crime title
 	private String mTitle;
+	//Date
+	private Date mDate;
+	//Time
+	private boolean mSolved;
 
 	public Crime() {
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public UUID getId() {
@@ -25,4 +31,19 @@ public class Crime {
 		mTitle = title;
 	}
 
+	public Date getmDate() {
+		return mDate;
+	}
+
+	public void setmDate(Date mDate) {
+		this.mDate = mDate;
+	}
+
+	public boolean ismSolved() {
+		return mSolved;
+	}
+
+	public void setmSolved(boolean mSolved) {
+		this.mSolved = mSolved;
+	}
 }
