c1a259ff3c216f4b70b29c7e7ba4841bf673c75e
Mohamed Elmekki <mohamed.elmekki@gmail.com>
2017-06-26 19:46:25 -0400

    Ch15 Pg289.

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeFragment.java
@@ -2,9 +2,14 @@ package criminalintent.android.bignerdranch.com.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.format.DateFormat;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -33,12 +38,15 @@ public class CrimeFragment extends Fragment {
 	//Request codes for target fragment (DatePickerFragment)
 	private static final int REQUEST_DATE = 0;
 	private static final int REQUEST_TIME = 1;
+	private static final int REQUEST_CONTACT = 1;
 
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
 	private Button mTimeButton;
 	private CheckBox mSolvedCheckBox;
+	private Button mReportButton;
+	private Button mSuspectButton;
 
 	/*
 	Accepts UUID
@@ -94,7 +102,7 @@ public class CrimeFragment extends Fragment {
 	}
 
 	/*
-	Called by host Activity
+	Called by host Activity (CrimeListFragment)
 	Explicitly inflates fragment view; returns fragment view to host Activity
 	 */
 	@Override
@@ -161,6 +169,53 @@ public class CrimeFragment extends Fragment {
 			}
 		});
 
+		mReportButton = (Button) v.findViewById(R.id.crime_report);
+		mReportButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*
+				//Define action of implicit intent
+				Intent i = new Intent(Intent.ACTION_SEND);
+				//Define data type
+				i.setType("text/plain");
+				//Package crime report as an intent extra
+				i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+				i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_suspect));
+				startActivity(i);
+				*/
+				ShareCompat.IntentBuilder.from(getActivity())
+						.setType("text/plain")
+						.setText(getCrimeReport())
+						.setSubject(getString(R.string.crime_report_suspect))
+						.setChooserTitle(getString(R.string.send_report))
+						.startChooser();
+			}
+		});
+
+		//Implicit intent asking for a contact
+		final Intent pickContact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+		mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
+		mSuspectButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Start contact picker
+				startActivityForResult(pickContact, REQUEST_CONTACT);
+			}
+		});
+		//Display suspect on button
+		if (mCrime.getSuspect() != null) {
+			mSuspectButton.setText(mCrime.getSuspect());
+		}
+
+		//Check which part of the OS called PackageManager
+		//Protects against not having a contacts app
+		PackageManager packageManager = getActivity().getPackageManager();
+		//.resolveActivity(intent) - determines how best handle the given intent
+		//If no valid app exists, the Suspect button is disabled on creation
+		if (packageManager.resolveActivity(pickContact, PackageManager.MATCH_DEFAULT_ONLY) == null) {
+			mSuspectButton.setEnabled(false);
+		}
+
 		return v;
 	}
 
@@ -222,6 +277,38 @@ public class CrimeFragment extends Fragment {
 			case REQUEST_TIME:
 				updateTime();
 				break;
+		}
+
+		if (requestCode == REQUEST_DATE) {
+			updateDate();
+		} else if (requestCode == REQUEST_TIME) {
+			updateTime();
+		} else if (requestCode == REQUEST_CONTACT && data == null) {
+			Uri contactUri = data.getData();
+
+			//Specify which fields query should return values for
+			String[] queryFields = new String[] {
+					//ContactsContract - access to contacts database
+					ContactsContract.Contacts.DISPLAY_NAME
+			};
+
+			//Perform query - contactUri is like a where clause
+			Cursor c = getActivity().getContentResolver().query(contactUri, queryFields, null, null, null);
+
+			try {
+				//Double-check that results were returned
+				if (c.getCount() == 0) {
+					return;
+				}
+
+				//Pull out the first column of the first row of data - suspect's name
+				c.moveToFirst();
+				String suspect = c.getString(0);
+				mCrime.setSuspect(suspect);
+				mSuspectButton.setText(suspect);
+			} finally {
+				c.close();
+			}
 		}
 	}
 
