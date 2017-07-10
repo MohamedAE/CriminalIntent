a3432339edeef20f20c30d675303c7bb248d1c37
Mohamed Elmekki <mohamed.elmekki@gmail.com>
2017-07-10 14:59:26 -0400

    Ch16 Pg311.

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package criminalintent.android.bignerdranch.com.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,19 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
 
 	protected abstract Fragment createFragment();
 
+    /*
+    Function to allow subclasses to override and return a layout other than the
+    default activity_fragment
+    */
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
 		//Instantiate new FragmentManager; explicit calls made from this object
 		FragmentManager fm = getSupportFragmentManager();
